64c8eefa25c9c9ae809c55f7a79649f9452ea806
Hashimmalik46 <hashimzahoor2003@gmail.com>
2026-02-01 18:37:29 +0530

    started learning react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from "./Nav.jsx";
+import Header from "./Header.jsx";
+function App() {
+  return (
+    <>
+      <Header />
+      <Nav />
+    </>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,4 @@
+function Header() {
+  return <h1 className="text-amber-300 text-3xl font-bold py-5">Hey There!</h1>;
+}
+export default Header;
