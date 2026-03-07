7b6b06112c451660d96d29a9edbdd0c5fd6cb3c3
Hashimmalik46 <hashimzahoor2003@gmail.com>
2026-03-07 23:56:20 +0530

    created a reusable star rating component

--- usePopcorn/src/main.jsx
+++ usePopcorn/src/main.jsx
@@ -3,10 +3,22 @@ import { createRoot } from "react-dom/client";
 // import './index.css';
 import App from "./App.jsx";
 import StarRating from "./components/StarRating.jsx";
+import Test from "./components/Test.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating="3"
+      size={35}
+      color="red"
+      className="test"
+      defaultRating={3}
+    />
+    <Test/>
   </StrictMode>,
 );
