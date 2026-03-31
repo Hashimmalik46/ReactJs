6c96e3d8ec6dfd06ee3e762538dd652a5d826b49
Hashimmalik46 <hashimzahoor2003@gmail.com>
2026-03-31 18:38:53 +0530

    auth implemented

--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -1,6 +1,8 @@
+import { useCities } from "../contexts/CitiesContext";
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
 function CityItem({ city }) {
+  const { currentCity, deleteCity } = useCities();
   const { cityName, flag, date, id, position } = city;
 
   const formatDate = (date) =>
@@ -10,13 +12,22 @@ function CityItem({ city }) {
       year: "numeric",
     }).format(new Date(date));
 
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+      <Link
+        className={`${styles.cityItem} ${id === currentCity.id ? styles["cityItem--active"] : ""}`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
         <img src={flag} className={styles.flag} />
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button className={styles.deleteBtn} onClick={handleClick}>
+          &times;
+        </button>
       </Link>
     </li>
   );
